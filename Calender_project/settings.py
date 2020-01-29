0de4b9264f29adea4214ebca4b3db7e822e5920e
kishan333 <kishanpanchal123.kp@gmail.com>
2020-01-29 16:43:19 +0530

    still trying to upload some folders.

--- Calender_project/settings.py
+++ Calender_project/settings.py
@@ -78,7 +78,6 @@ WSGI_APPLICATION = 'Calender_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -96,7 +95,6 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -115,24 +113,16 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = 'viewEvent'
-
 WEBPACK_LOADER = {
     'DEFAULT': {
         'CACHE': not DEBUG,
